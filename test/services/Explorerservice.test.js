4b6d4e7b93796e3acced05e36320b2646b8b7e6b
Oscar Gutierrez <oscar_yo_123@hotmail.com>
2022-05-03 15:07:02 -0500

    Probando el metodo filterByMission de Explorers service

---
+++ test/services/Explorerservice.test.js
@@ -0,0 +1,28 @@
+const Reader = require("../../lib/utils/reader");
+const explorers = Reader.readJsonFile("explorers.json");
+const ExplorerService = require("../../lib/services/Explorerservice");
+
+describe("Pruebas para el ExplorerService", () => {
+  test("Regresando el primer explorer que esta en java", () => {
+    const explorersInJava = ExplorerService.filterByMission(explorers, "java");
+
+    expect(explorersInJava[0]).toMatchObject({
+      name: "Woopa6",
+      githubUsername: "ajolonauta6",
+      score: 6,
+      mission: "java",
+      stacks: ["elm"],
+    });
+  });
+  test("Regresando el ultimo explorer que esta en node", () => {
+    const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+
+    expect(explorersInNode[9]).toMatchObject({
+      name: "Woopa15",
+      githubUsername: "ajolonauta15",
+      score: 15,
+      mission: "node",
+      stacks: ["javascript", "elixir", "groovy", "reasonML", "elm"],
+    });
+  });
+});
