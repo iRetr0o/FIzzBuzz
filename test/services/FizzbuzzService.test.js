9ddaf7b9b80b41f37c4ff33b8fc0590bd39504e8
Oscar Gutierrez <oscar_yo_123@hotmail.com>
2022-05-03 16:27:37 -0500

    Agregando prueba sobre si hay una propiedad llamada trick y cambiando a toEqual

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,12 +1,19 @@
 const FizzbuzzService = require("../../lib/services/FizzbuzzService");
 
 describe("Prueba FizzBuzz", () => {
-  test("Agregando trick", () => {
-    const explorerP = { name: "Explorer1", score: 1 };
+  test("Ver si se agrega la propiedad trick", () => {
+    const explorerP1 = { name: "ExplorerP1", score: 0 };
+    const explorerP1Trick =
+      FizzbuzzService.applyValidationInExplorer(explorerP1);
+
+    expect(explorerP1Trick).toHaveProperty("trick");
+  });
+  test("Comprobando las propiedades del explorer", () => {
+    const explorerP = { name: "ExplorerP", score: 1 };
     const explorerTrick = FizzbuzzService.applyValidationInExplorer(explorerP);
 
-    expect(explorerTrick).toMatchObject({
-      name: "Explorer1",
+    expect(explorerTrick).toEqual({
+      name: "ExplorerP",
       score: 1,
       trick: 1,
     });
