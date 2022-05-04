ee111580c52be24ed1d491f7a185e7d5c162bce1
Oscar Gutierrez <oscar_yo_123@hotmail.com>
2022-05-03 21:37:08 -0500

    Agregando formato a mis archivos con linter-fix

--- test/services/Explorerservice.test.js
+++ test/services/Explorerservice.test.js
@@ -3,69 +3,69 @@ const explorers = Reader.readJsonFile("explorers.json");
 const ExplorerService = require("../../lib/services/Explorerservice");
 
 describe("Pruebas para el ExplorerService", () => {
-  test("Regresando el primer explorer que esta en java", () => {
-    const explorersInJava = ExplorerService.filterByMission(explorers, "java");
+    test("Regresando el primer explorer que esta en java", () => {
+        const explorersInJava = ExplorerService.filterByMission(explorers, "java");
 
-    expect(explorersInJava[0]).toMatchObject({
-      name: "Woopa6",
-      githubUsername: "ajolonauta6",
-      score: 6,
-      mission: "java",
-      stacks: ["elm"],
+        expect(explorersInJava[0]).toMatchObject({
+            name: "Woopa6",
+            githubUsername: "ajolonauta6",
+            score: 6,
+            mission: "java",
+            stacks: ["elm"],
+        });
     });
-  });
-  test("Regresando el ultimo explorer que esta en node", () => {
-    const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+    test("Regresando el ultimo explorer que esta en node", () => {
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
 
-    expect(explorersInNode[9]).toMatchObject({
-      name: "Woopa15",
-      githubUsername: "ajolonauta15",
-      score: 15,
-      mission: "node",
-      stacks: ["javascript", "elixir", "groovy", "reasonML", "elm"],
+        expect(explorersInNode[9]).toMatchObject({
+            name: "Woopa15",
+            githubUsername: "ajolonauta15",
+            score: 15,
+            mission: "node",
+            stacks: ["javascript", "elixir", "groovy", "reasonML", "elm"],
+        });
     });
-  });
-  test("Cantidad de explorers en java", () => {
-    const javaExplorers = ExplorerService.getAmountOfExplorersByMission(
-      explorers,
-      "java"
-    );
+    test("Cantidad de explorers en java", () => {
+        const javaExplorers = ExplorerService.getAmountOfExplorersByMission(
+            explorers,
+            "java"
+        );
 
-    expect(javaExplorers).toBe(5);
-  });
-  test("Cantidad de explorers en node", () => {
-    const nodeExplorers = ExplorerService.getAmountOfExplorersByMission(
-      explorers,
-      "node"
-    );
+        expect(javaExplorers).toBe(5);
+    });
+    test("Cantidad de explorers en node", () => {
+        const nodeExplorers = ExplorerService.getAmountOfExplorersByMission(
+            explorers,
+            "node"
+        );
 
-    expect(nodeExplorers).toBe(10);
-  });
-  test("Usernames de explorers que estan en node", () => {
-    const usersInNode = ExplorerService.getExplorersUsernamesByMission(
-      explorers,
-      "node"
-    );
+        expect(nodeExplorers).toBe(10);
+    });
+    test("Usernames de explorers que estan en node", () => {
+        const usersInNode = ExplorerService.getExplorersUsernamesByMission(
+            explorers,
+            "node"
+        );
 
-    expect(usersInNode).toMatchObject([
-      "ajolonauta1",
-      "ajolonauta2",
-      "ajolonauta3",
-      "ajolonauta4",
-      "ajolonauta5",
-      "ajolonauta11",
-      "ajolonauta12",
-      "ajolonauta13",
-      "ajolonauta14",
-      "ajolonauta15",
-    ]);
-  });
-  test("Probando que sea un array", () => {
-    const usersInJava = ExplorerService.getExplorersUsernamesByMission(
-      explorers,
-      "java"
-    );
+        expect(usersInNode).toMatchObject([
+            "ajolonauta1",
+            "ajolonauta2",
+            "ajolonauta3",
+            "ajolonauta4",
+            "ajolonauta5",
+            "ajolonauta11",
+            "ajolonauta12",
+            "ajolonauta13",
+            "ajolonauta14",
+            "ajolonauta15",
+        ]);
+    });
+    test("Probando que sea un array", () => {
+        const usersInJava = ExplorerService.getExplorersUsernamesByMission(
+            explorers,
+            "java"
+        );
 
-    expect(usersInJava).toHaveLength(5);
-  });
+        expect(usersInJava).toHaveLength(5);
+    });
 });
--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,46 +1,46 @@
 const FizzbuzzService = require("../../lib/services/FizzbuzzService");
 
 describe("Prueba FizzBuzz", () => {
-  test("Ver si se agrega la propiedad trick", () => {
-    const explorerP1 = { name: "ExplorerP1", score: 0 };
-    const explorerP1Trick =
+    test("Ver si se agrega la propiedad trick", () => {
+        const explorerP1 = { name: "ExplorerP1", score: 0 };
+        const explorerP1Trick =
       FizzbuzzService.applyValidationInExplorer(explorerP1);
 
-    expect(explorerP1Trick).toHaveProperty("trick");
-  });
-  test("Comprobando las propiedades del explorer", () => {
-    const explorerP = { name: "ExplorerP", score: 7 };
-    const explorerTrick = FizzbuzzService.applyValidationInExplorer(explorerP);
-
-    expect(explorerTrick).toEqual({
-      name: "ExplorerP",
-      score: 7,
-      trick: 7,
+        expect(explorerP1Trick).toHaveProperty("trick");
     });
-  });
-  test("Trick = score si el resto entre 5 y 3 no es 0", () => {
-    const explorer1 = { name: "Explorer1", score: 1 };
-    const explorerTrick = FizzbuzzService.applyValidationInExplorer(explorer1);
+    test("Comprobando las propiedades del explorer", () => {
+        const explorerP = { name: "ExplorerP", score: 7 };
+        const explorerTrick = FizzbuzzService.applyValidationInExplorer(explorerP);
 
-    expect(explorerTrick.trick).toBe(1);
-  });
-  test("Trick = FIZZ (resto entre 3 = 0)", () => {
-    const explorer3 = { name: "Explorer3", score: 3 };
-    const explorer3Trick = FizzbuzzService.applyValidationInExplorer(explorer3);
+        expect(explorerTrick).toEqual({
+            name: "ExplorerP",
+            score: 7,
+            trick: 7,
+        });
+    });
+    test("Trick = score si el resto entre 5 y 3 no es 0", () => {
+        const explorer1 = { name: "Explorer1", score: 1 };
+        const explorerTrick = FizzbuzzService.applyValidationInExplorer(explorer1);
 
-    expect(explorer3Trick.trick).toBe("FIZZ");
-  });
-  test("Trick = BUZZ (resto entre 5 = 0)", () => {
-    const explorer5 = { name: "Explorer5", score: 5 };
-    const explorer5Trick = FizzbuzzService.applyValidationInExplorer(explorer5);
+        expect(explorerTrick.trick).toBe(1);
+    });
+    test("Trick = FIZZ (resto entre 3 = 0)", () => {
+        const explorer3 = { name: "Explorer3", score: 3 };
+        const explorer3Trick = FizzbuzzService.applyValidationInExplorer(explorer3);
 
-    expect(explorer5Trick.trick).toBe("BUZZ");
-  });
-  test("Trick FIZZBUZZ (resto entre 5 y 3 = 0", () => {
-    const explorer15 = { name: "Explorer15", score: 15 };
-    const explorer15Trick =
+        expect(explorer3Trick.trick).toBe("FIZZ");
+    });
+    test("Trick = BUZZ (resto entre 5 = 0)", () => {
+        const explorer5 = { name: "Explorer5", score: 5 };
+        const explorer5Trick = FizzbuzzService.applyValidationInExplorer(explorer5);
+
+        expect(explorer5Trick.trick).toBe("BUZZ");
+    });
+    test("Trick FIZZBUZZ (resto entre 5 y 3 = 0", () => {
+        const explorer15 = { name: "Explorer15", score: 15 };
+        const explorer15Trick =
       FizzbuzzService.applyValidationInExplorer(explorer15);
 
-    expect(explorer15Trick.trick).toBe("FIZZBUZZ");
-  });
+        expect(explorer15Trick.trick).toBe("FIZZBUZZ");
+    });
 });
